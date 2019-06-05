f128b28f4dfcacab205a5634de6f5aa180f362d8
Shanin Roman <shanin1000@yandex.ru>
2019-06-05 23:05:04 +0300

    minor change in database methods

--- server/database/db_module.py
+++ server/database/db_module.py
@@ -31,12 +31,7 @@ class ReadRecord:
             async with self.db.pool.acquire() as conn:
                 async with conn.cursor(aiomysql.DictCursor) as cur:
                     await cur.execute(req)
-                    answer = []
-                    while True:
-                        row = await cur.fetchone()
-                        if row is None:
-                            break
-                        answer.append(row)
+                    answer = await cur.fetchall()
             return answer
 
 class STSDataBase:
@@ -55,19 +50,87 @@ class STSDataBase:
         await self.pool.wait_closed()
     
     
-    async def insert_record(self, table, columns, data):
+    async def insert_record(self, table, row):
         """
         table : str - имя таблицы
-        сolumns : str - имена стобцов, в которые будут записаны новые значени.
-        data : list - список новых строк на дополнение
+        row : {'column_name' : value, ...}
+        """
+        columns = [key for key in row if (row[key] != "None" or row[key] != "")]
+        values =  ['{!r}'.format(row[key]).replace("'", '"') for key in row if (row[key] != "None" or row[key] != "")]
+        if columns:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor(aiomysql.DictCursor) as cur:
+                    req = """
+                          INSERT INTO {} ({})
+                          VALUES({})
+                          """.format(table, " , ".join(columns), 
+                          " , ".join(values))
+                    
+                    await cur.execute(req)
+                    await conn.commit()
+                    print(await cur.fetchall())
+                
+    async def delete_record(self, table, row):
+        """
+        Удалить одну запись.
+        table : str - имя таблицы
+        row : {'column_name' : value, ...}
         """
         async with self.pool.acquire() as conn:
+            eq = "{} = {!r}"
+            condition = " AND ".join([eq.format(key, row[key]).replace("'", '"')  for key in row if (row[key] != "None" and row[key] != "")])
+            if condition:
+                async with conn.cursor(aiomysql.DictCursor) as cur:
+                    req = """
+                          Delete from {} where {}
+                          """.format(table, condition)
+                    await cur.execute(req)
+                    await conn.commit()
+                
+    async def delete_records(self, table, rows):
+        """
+        Удалить много записей.
+        table : str - имя таблицы
+        rows : [{'column_name' : value, ...}, ...]
+        """
+        async with self.pool.acquire() as conn:
+            eq = "{} = {!r}"
+            condition = []
+            for row in rows:
+                record_condition = "({})".format(" AND ".join([eq.format(key, row[key]).replace("'", '"')  for key in row if (row[key] != "None" and row[key] != "")]))
+                if record_condition != "()": 
+                    condition.append(record_condition)
+            condition = " OR ".join(condition)
+            if condition:
+                async with conn.cursor(aiomysql.DictCursor) as cur:
+                    req = """
+                          Delete from {} where {}
+                          """.format(table, condition)
+                    await cur.execute(req)
+                    await conn.commit()
+                    print(await cur.fetchall())
+                
+    async def update_record(self, table, row):
+        """
+        Обновить запись в таблице.
+        table : str - имя таблицы
+        row : {'column_name' : value, ...}
+        """
+        async with self.pool.acquire() as conn:
+            eq = "{} = {!r}"
+            pkey = self.findPkey(row)
+            condition =  "{} = {!r}".format(pkey, row[pkey]).replace("'", '"')
+            updatedRow = " , ".join([eq.format(key, row[key]).replace("'", '"')  for key in row if (row[key] != "None" and row[key] != "")])
             async with conn.cursor(aiomysql.DictCursor) as cur:
                 req = """
-                      INSERT INTO {} ({})
-                      VALUES({})
-                      """.format(table, " , ".join(list(map(str,columns))), 
-                      " , ".join(list(map(str, data))))
-                
+                      Update  table {} set {} where {}
+                      """.format(table, updatedRow, condition)
                 await cur.execute(req)
                 await conn.commit()
+                print(await cur.fetchall())
+                
+    
+    def findPkey(self, row):
+        for key in row:
+            if "_id" in key:
+                return key
